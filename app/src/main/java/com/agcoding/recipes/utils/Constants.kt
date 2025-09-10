1b9e44fe5627b2850234ecb29a6868a270a2b630
Ayush Gupta <59391849+Dark-Coders995@users.noreply.github.com>
2025-09-10 11:16:23 +0530

    Update Constants.kt

--- app/src/main/java/com/agcoding/recipes/utils/Constants.kt
+++ app/src/main/java/com/agcoding/recipes/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.agcoding.recipes.utils
 
 object Constants {
     const val BASE_URL = "https://api.spoonacular.com/recipes/"
-    const val API_KEY = "5f80e9009665486395641e8ee79afa9f"
+    const val API_KEY = "{API_KEY}"
 }
